aa2155687f771ab1cedf4e235cfa1448e726c20f
Le Thanh Hoang Anh <lethanhhoanganh4@gmail.com>
2025-12-02 11:20:42 +0700

    update

---
+++ database/migrations/2025_12_02_041332_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id'); //id sản phẩm
+            $table->string('product_name',100); //tên sản phẩm
+            $table->foreignId('brand_id')->constrained('brands')->onDelete('cacade'); // ID thương hiệu + khóa ngoại tới brands
+            $table->string('category',50); // Danh muc
+            $table->decimal('price',12,2); //Giá bán
+            $table->decimal('cost_price',12,2); //Giá nhập
+            $table->integer('stock');
+            $table->enum('status',[''])->default();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
