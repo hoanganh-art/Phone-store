0a09e8aae74b67f6dd02c15c270d1213925ca878
Le Thanh Hoang Anh <lethanhhoanganh4@gmail.com>
2025-12-02 11:12:58 +0700

    update

---
+++ database/migrations/2025_12_02_040905_create_brands_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id('brand_id'); //id thương hiệu
+            $table->string('brand_name',50); //Tên thương hiệu
+            $table->string('country',50); //Quốc gia
+            $table->text('description')->nullable(); //Mô tả
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
