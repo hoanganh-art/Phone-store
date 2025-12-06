66681679fec2309ba3d2688231d52153e69a2260
Hoang <lethanhoanganh4@gmail.com>
2025-12-06 08:35:47 +0700

    update

--- app/Models/invoices.php
+++ app/Models/invoices.php
@@ -7,4 +7,41 @@ use Illuminate\Database\Eloquent\Model;
 class invoices extends Model
 {
     //
+    protected $table = "invoices";
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'customer_id',
+        'employee_id',
+        'invoice_date',
+        'subtotal',
+        'discount',
+        'total_amount',
+        'payment_method',
+        'status',
+    ];
+
+    protected $casts = [
+        'invoice_date' => 'datetime',
+        'subtotal' => 'decimal:2',
+        'discount' => 'decimal:2',
+        'total_amount' => 'decimal:2'
+    ];
+
+    // Quan hệ: Một hóa đơn thuộc về một khách hàng
+    public function customer()
+    {
+        return $this->belongsTo(customers::class, 'customer_id');
+    }
+
+    // Quan hệ: Một hóa đơn thuộc về một nhân viên
+    public function employee()
+    {
+        return $this->belongsTo(employees::class, 'employee_id');
+    }
+
+      // Quan hệ: Một hóa đơn có nhiều chi tiết hóa đơn
+    public function invoiceDetails()
+    {
+        return $this->hasMany(invoice_details::class, 'invoice_id');
+    }
 }
