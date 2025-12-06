a936d2460830cef854b2dc279f32c2620942470e
Hoang <lethanhoanganh4@gmail.com>
2025-12-06 08:22:06 +0700

    update

--- app/Models/brands.php
+++ app/Models/brands.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class brands extends Model
 {
     //
+    protected $table = "bands";
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'brand_name',
+        'country',
+        'description',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(products::class, 'brand_id', 'id');
+    }
 }
--- app/Models/products.php
+++ app/Models/products.php
@@ -2,9 +2,51 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Validation\Rules\In;
 
 class products extends Model
 {
     //
+    protected $table = "products";
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'product_name',
+        'brand_id',
+        'category',
+        'ram',
+        'storage',
+        'price',
+        'cost_price',
+        'stock',
+        'status',
+        'created_at'
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'cost_price' => 'decimal:2',
+        'created_at' => 'datetime'
+    ];
+
+    // Quan hệ: Sản phẩm thuộc về một thương hiệu
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(brands::class, 'brand_id');
+    }
+    // Quan hệ: Một sản phẩm có nhiều chi tiết hóa đơn
+    public function invoiceDetails(): HasMany
+    {
+        return $this->hasMany(invoice_details::class, 'product_id');
+    }
+
+    // Quan hệ: Một sản phẩm có nhiều chi tiết nhập hàng
+    public function importDetails(): HasMany
+    {
+        return $this->hasMany(import_details::class, 'product_id');
+    }
+
 }
